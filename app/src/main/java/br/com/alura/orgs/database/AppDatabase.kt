0bdfedfc72455b13d47dfadc066f04812513ac28
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-12-17 14:42:54 -0300

    Implementando Migrations

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -7,11 +7,13 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import br.com.alura.orgs.database.converter.Converters
 import br.com.alura.orgs.database.dao.ProdutoDao
+import br.com.alura.orgs.database.dao.UsuarioDao
 import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.model.Usuario
 
 @Database(
     entities = [
-        Produto::class
+        Produto::class,
     ],
     version = 1,
     exportSchema = true
@@ -20,6 +22,8 @@ import br.com.alura.orgs.model.Produto
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun produtoDao(): ProdutoDao
+
+  //  abstract fun usuarioDao(): UsuarioDao
 
     companion object {
         @Volatile
